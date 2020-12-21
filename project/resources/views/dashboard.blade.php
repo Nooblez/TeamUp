a13ed65d3f50ec75e90c8a607760e501b3f8a1d8
adriano <adripalumbo95@rocketmail.com>
2020-12-21 17:02:38 +0100

    primi cambi iniziali al layout

--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+               DASHBOARD UTENTE LOGGATO - GRUPPI PERSONALI, EVENTUALE "CASCATA" DI GRUPPI, ETC
             </div>
         </div>
     </div>
--- project/routes/web.php
+++ project/routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
